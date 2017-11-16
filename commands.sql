861e852c3ba56fd892c6197806ad4c4efe6380a5
Hugh Morrison <hmorri32@gmail.com>
2017-11-16 16:14:31 -0700

    inner and outer joins

--- commands.sql
+++ commands.sql
@@ -23,17 +23,19 @@ SELECT count(column_name) FROM table_name;
 -- Write queries for the following:
 
 -- Return all main courses. Hint: What ActiveRecord method would you use to get this?
-SELECT * from items where course = 'main';
+SELECT * FROM items WHERE course = 'main';
 
 -- Return only the names of the main courses.
-SELECT name from items where course = 'main';
+SELECT NAME FROM items WHERE course = 'main';
 
 -- Return the min and max value for the main courses.
-SELECT max(revenue) from items where course = 'main';
-SELECT min(revenue) from items where course = 'main';
+SELECT max(revenue) FROM items WHERE course = 'main';
+SELECT max(revenue), min(revenue) from items WHERE course = 'main';
+SELECT min(revenue) FROM items WHERE course = 'main';
+SELECT avg(revenue) FROM items WHERE course = 'main';
 
 -- What's the total revenue for all main courses?
-SELECT sum(revenue) from items where course = 'main';
+SELECT sum(revenue) FROM items WHERE course = 'main';
 
 -- INNER JOINS
 CREATE TABLE seasons(id SERIAL, name TEXT);
@@ -138,3 +140,110 @@ WHERE item_categories.category_id = item_categories.item_id AND items.name = 'ar
 --  arugula salad | vegetarian
 --  arugula salad | lunch
 --  arugula salad | side
+
+-- Outer Joins
+
+INSERT INTO items (name, revenue, season_id)
+VALUES ('italian beef', 600, NULL),
+       ('cole slaw', 150, NULL),
+       ('ice cream sandwich', 700, NULL);
+
+SELECT i.name items, s.name seasons
+FROM items i
+INNER JOIN seasons s
+ON i.season_id = s.id;
+
+-- items                | seasons
+-- ---------------------+---------
+-- hot dog              | summer
+-- veggie lasagna       | summer
+-- striped bass         | summer
+-- burger               | summer
+-- arugula salad        | autumn
+-- lobster mac n cheese | winter
+-- grilled cheese       | spring
+
+SELECT *
+FROM items i
+LEFT OUTER JOIN seasons s
+ON i.season_id = s.id;
+
+--  id |         name         | revenue | season_id | id |  name
+-- ----+----------------------+---------+-----------+----+--------
+--   4 | striped bass         |     500 |         1 |  1 | summer
+--   2 | bish lasagna         |    1000 |         1 |  1 | summer
+--   3 | veggie lasagna       |    1000 |         1 |  1 | summer
+--   5 | burger               |    2000 |         1 |  1 | summer
+--   7 | hot dog              |    1000 |         1 |  1 | summer
+--   8 | arugula salad        |    1100 |         2 |  2 | autumn
+--   1 | lobster mac n cheese |    1200 |         3 |  3 | winter
+--   6 | grilled cheese       |     800 |         4 |  4 | spring
+--   9 | italian beef         |     600 |           |    |
+--  10 | cole slaw            |     150 |           |    |
+--  11 | ice cream sandwich   |     700 |           |    |
+
+SELECT * FROM items i RIGHT OUTER JOIN seasons s ON i.season_id = s.id;
+
+--  id |         name         | revenue | season_id | id |  name
+-- ----+----------------------+---------+-----------+----+--------
+--   4 | striped bass         |     500 |         1 |  1 | summer
+--   2 | bish lasagna         |    1000 |         1 |  1 | summer
+--   3 | veggie lasagna       |    1000 |         1 |  1 | summer
+--   5 | burger               |    2000 |         1 |  1 | summer
+--   7 | hot dog              |    1000 |         1 |  1 | summer
+--   8 | arugula salad        |    1100 |         2 |  2 | autumn
+--   1 | lobster mac n cheese |    1200 |         3 |  3 | winter
+--   6 | grilled cheese       |     800 |         4 |  4 | spring
+
+
+-- Sub queries sup
+-- Write a WHERE clause that returns the items that have a revenue greater than that average.
+
+SELECT * FROM items WHERE revenue > (SELECT AVG(revenue) FROM items);
+
+--  id |         name         | revenue | season_id
+-- ----+----------------------+---------+-----------
+--   1 | lobster mac n cheese |    1200 |         3
+--   2 | bish lasagna         |    1000 |         1
+--   3 | veggie lasagna       |    1000 |         1
+--   5 | burger               |    2000 |         1
+--   7 | hot dog              |    1000 |         1
+--   8 | arugula salad        |    1100 |         2
+
+-- Write a query that returns the sum of all items that have a category of dinner.
+SELECT SUM(i.revenue)
+FROM items i
+INNER JOIN item_categories ic
+ON i.id = ic.item_id
+INNER JOIN categories c
+ON c.id = ic.category_id
+WHERE c.name = 'dinner';
+
+--  sum
+-- ------
+--  3000
+
+
+-- Write a query that returns the sum of all items for each category. The end result should look like this:
+SELECT c.name, SUM(i.revenue)
+FROM categories c
+INNER JOIN item_categories ic
+ON c.id = ic.category_id
+INNER JOIN items i
+ON i.id = ic.item_id
+GROUP BY c.name;
+
+SELECT categories.name, SUM(items.revenue)
+FROM categories
+INNER JOIN item_categories ON categories.id = item_categories.category_id
+INNER JOIN items ON items.id = item_categories.item_id
+GROUP BY categories.name
+
+--     name    | sum
+-- ------------+------
+--  dinner     | 3000
+--  vegetarian | 4000
+--  lunch      | 3500
+--  side       | 2200
+
+-- Write a query that returns the sum of all items for each category. The end result should look like this:
